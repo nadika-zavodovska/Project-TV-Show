054130420b440e2e76c7e0326836f535e5fbb82f
nadika <nadikazavodovska@gmail.com>
2024-12-18 15:28:23 +0000

    Add html structure

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
+
+const allEpisodes = getAllEpisodes();
+
+const header = document.getElementById('header');
+console.log(header);
+console.log(allEpisodes[0]['name']);
 
 window.onload = setup;
